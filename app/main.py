92ceafe35a2223b23ccf53c13d0692d1b297ca00
james audretsch <jamesaud@gmail.com>
2018-09-28 10:12:13 +0300

    quick p

--- app/main.py
+++ app/main.py
@@ -1,9 +1,38 @@
-from flask import Flask
+from flask import Flask, request
+from flask_restful import Resource, Api
+from flask_pymongo import PyMongo
+from bson import json_util
+
 app = Flask(__name__)
+api = Api(app)
+app.config["MONGO_URI"] = "mongodb://mongo:27017/my-database"
+mongo = PyMongo(app)
+
+
+class User(Resource):
+    def get(self, user_id):
+        return "Success"
+
+    def put(self, user_id):
+        json = request.get_json()
+        user = {
+             "name": json["name"],
+             "email": json["email"],
+             "password": json["password"],
+             "id": hash(json["email"])
+        }
+        user = mongo.db.users.insert_one(user)
+        return {"user_id": str(user.inserted_id)}
+
 
 @app.route("/")
 def hello():
-    return "Hello World from Flask wow"
+    users = list(mongo.db.users.find())
+    return json_util.dumps(users)
+
+
+api.add_resource(User, '/user/<string:user_id>')
+
 
 if __name__ == "__main__":
     # Only for debugging while developing
